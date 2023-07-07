5d34d67b545f3b5358ccd887e3adda085163552d
Daniel Paredes <danielparedesmilian@gmail.com>
2023-07-06 23:18:29 -0500

    added controllers

---
+++ Backend/Routes/goalRoutes.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const router = express.Router();
+const { getGoals } = require("../controllers/goalController");
+
+router.get("/", getGoals);
+router.post("/", (req, res) => {
+	res.status(200).json({ message: "Set Goal" });
+});
+router.put("/:id", (req, res) => {
+	res.status(200).json({ message: `Update Goals${req.params.id}` });
+});
+router.delete("/:id", (req, res) => {
+	res.status(200).json({ message: `Delete Goals${req.params.id}` });
+});
+
+module.exports = router;
