6fd90ceb09b62792007569fb9865dbc8ba969e55
Daniel Paredes <danielparedesmilian@gmail.com>
2023-07-25 12:50:05 -0500

    Goals Controller and routes setup

--- Backend/middleware/errorMiddleware.js
+++ Backend/middleware/errorMiddleware.js
@@ -2,4 +2,13 @@ const errorHandler = (err, req, res, next) => {
 	const statusCode = res.statusCode ? res.statusCode : 500;
 
 	res.status;
+
+	res.json({
+		message: error.message,
+		stack: process.env.NODE_ENV === "production" ? null : err.stack,
+	});
+};
+
+module.exports = {
+	errorHandler,
 };
