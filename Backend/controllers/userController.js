99b87ca3e2fdea5341e68ade084b453dc6f4b9af
Daniel Paredes <danielparedesmilian@gmail.com>
2023-07-29 06:50:55 -0500

    added user routes

---
+++ Backend/controllers/userController.js
@@ -0,0 +1,35 @@
+// @desc  Register New User
+// @route POST /api/users
+// @access Public
+
+const registerUser = (req, res) => {
+	res.json({
+		message: "Register User",
+	});
+};
+
+// @desc  Authenticate a User
+// @route POST /api/users/login
+// @access Public
+
+const loginUser = (req, res) => {
+	res.json({
+		message: "login User",
+	});
+};
+
+// @desc  Get user data
+// @route GET /api/users/me
+// @access Public
+
+const getMe = (req, res) => {
+	res.json({
+		message: "User data display",
+	});
+};
+
+module.exports = {
+	registerUser,
+	loginUser,
+	getMe,
+};
