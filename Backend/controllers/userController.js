931edccd4a8a8ac17b9b53573fbb9c39c80953d3
Daniel Paredes <danielparedesmilian@gmail.com>
2023-08-15 14:22:25 -0500

    aadded

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -1,32 +1,38 @@
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
+const asyncHandler = register("express-async-handler");
+const User = require("../models/userModel");
+
 // @desc  Register New User
 // @route POST /api/users
 // @access Public
 
-const registerUser = (req, res) => {
+const registerUser = asyncHandler(async (req, res) => {
+	const { name, email, password } = req.body;
 	res.json({
 		message: "Register User",
 	});
-};
+});
 
 // @desc  Authenticate a User
 // @route POST /api/users/login
 // @access Public
 
-const loginUser = (req, res) => {
+const loginUser = asyncHandler(async (req, res) => {
 	res.json({
 		message: "login User",
 	});
-};
+});
 
 // @desc  Get user data
 // @route GET /api/users/me
 // @access Public
 
-const getMe = (req, res) => {
+const getMe = asyncHandler(async (req, res) => {
 	res.json({
 		message: "User data display",
 	});
-};
+});
 
 module.exports = {
 	registerUser,
