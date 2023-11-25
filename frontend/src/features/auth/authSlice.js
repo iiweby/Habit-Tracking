72fd3a66b8c22493fff9e3498129da9bb477ec5b
Daniel Paredes <danielparedesmilian@gmail.com>
2023-11-25 17:41:19 -0600

    Created Register Function

--- frontend/src/features/auth/authSlice.js
+++ frontend/src/features/auth/authSlice.js
@@ -1,4 +1,5 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import authService from "./authService";
 
 // Get user from localStorage
 
@@ -11,6 +12,24 @@ const initialState = {
 	isLoading: false,
 	message: "",
 };
+
+//Register user
+export const register = createAsyncThunk(
+	"auth/register",
+	async (user, thunkAPI) => {
+		try {
+			return await authService.register(user);
+		} catch (error) {
+			const message =
+				(error.response &&
+					error.response.data &&
+					error.response.data.message) ||
+				error.message ||
+				error.toString();
+			return thunkAPI.rejectWithValue(message);
+		}
+	}
+);
 
 export const authSlice = createSlice({
 	name: "auth",
